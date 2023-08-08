19ee40ffe0e5cdccc5e129558e99d0c893accf6d
chans1128 <happyshin99@naver.com>
2023-08-09 00:40:28 +0900

    [START] gyg web project

---
+++ src/main/java/gyg/gygweb/GygWebApplication.java
@@ -0,0 +1,13 @@
+package gyg.gygweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GygWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GygWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gyg/gygweb/GygWebApplicationTests.java
@@ -0,0 +1,13 @@
+package gyg.gygweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GygWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
